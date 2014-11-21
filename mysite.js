4607b62957957071b79a50d41f51f17b31887a44
jgzuke <jgzuke@gmail.com>
2014-11-21 17:43:27 -0500

    Need to add projects and find a place to host

--- mysite.js
+++ mysite.js
@@ -1,7 +1,6 @@
 $(window).bind('scroll',function(e){
     parallaxScroll();
 });
-
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     $('#backgrounddiv').css('top',(0-(scrolled*.5))+'px');
