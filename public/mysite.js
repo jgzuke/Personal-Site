58427fcef6b8b2847bcd2f1d36de6c5471fb7e11
jgzuke <jgzuke@gmail.com>
2014-11-26 04:33:13 -0500

    got smooth scroll

--- public/mysite.js
+++ public/mysite.js
@@ -7,3 +7,13 @@ function parallaxScroll()
     var scrolled = $(window).scrollTop();
     $('#backgrounddiv').css('top',(0-(scrolled*.5))+'px');
 }
+$('#nextpage').click(function()
+{
+
+    $('html, body').animate({scrollTop: $('#scrolling').offset().top}, 'slow');
+});
+$('#prevpage').click(function()
+{
+	
+    $('html, body').animate({scrollTop: $('#scrolling').offset().top}, 'slow');
+});
