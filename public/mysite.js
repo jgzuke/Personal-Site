ad8f6fd44246c86c0e514135d6c6b96da72f9278
jgzuke <jgzuke@gmail.com>
2014-11-27 05:06:28 -0500

    Clicking, fading, everything for images works

--- public/mysite.js
+++ public/mysite.js
@@ -64,48 +64,65 @@ $(document).ready(function()
 	$('.project2slide').first().addClass('active2');
 	$('.project2slide').hide();
 	$('.active2').show();
-	$('#project1describe').hide();
-	$('#project2describe').hide();
+
+	$('.project1describe').first().next().addClass('active1Describe');
+	$('.project2describe').first().next().addClass('active2Describe');
+	$('.project1describe').hide();
+	$('.project2describe').hide();
 });
 $('#button-next1').click(function()
 {
 	$('.active1').removeClass('active1').addClass('oldActive');
+	$('.active1Describe').removeClass('active1Describe').addClass('oldActiveDescribe');
 	if ($('.oldActive').is(':last-child'))
 	{
 		$('.project1slide').first().addClass('active1');
+		$('.project1describe').first().addClass('active1Describe');
 	} else
 	{
 		$('.oldActive').next().addClass('active1');
+		$('.oldActiveDescribe').next().addClass('active1Describe');
 	}
 	$('.oldActive').removeClass('oldActive');
+	$('.oldActiveDescribe').removeClass('oldActiveDescribe');
 	$('.project1slide').fadeOut();
+	$('.mousedOver1').fadeOut();
 	$('.active1').fadeIn();
 });
 $('#button-next2').click(function()
 {
 	$('.active2').removeClass('active2').addClass('oldActive');
+	$('.active2Describe').removeClass('active2Describe').addClass('oldActiveDescribe');
 	if ($('.oldActive').is(':last-child'))
 	{
 		$('.project2slide').first().addClass('active2');
+		$('.project2describe').first().addClass('active2Describe');
 	} else
 	{
 		$('.oldActive').next().addClass('active2');
+		$('.oldActiveDescribe').next().addClass('active2Describe');
 	}
 	$('.oldActive').removeClass('oldActive');
+	$('.oldActiveDescribe').removeClass('oldActiveDescribe');
 	$('.project2slide').fadeOut();
+	$('.mousedOver2').fadeOut();
 	$('.active2').fadeIn();
 });
 $('#project1').mouseenter(function()
 {
-    $('#project1describe').fadeIn();
+    $('.active1Describe').fadeIn();
+    $('.active1Describe').addClass('mousedOver1');
 }).mouseleave(function()
 {
-    $('#project1describe').fadeOut();
+    $('.active1Describe').fadeOut();
+    $('.active1Describe').removeClass('mousedOver1');
 });
 $('#project2').mouseenter(function()
 {
-    $('#project2describe').fadeIn();
+    $('.active2Describe').fadeIn();
+	$('.active2Describe').addClass('mousedOver2');
 }).mouseleave(function()
 {
-    $('#project2describe').fadeOut();
+    $('.active2Describe').fadeOut();
+    $('.active2Describe').removeClass('mousedOver2');
 });
